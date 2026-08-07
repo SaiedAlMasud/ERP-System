f04a995791448b4f3cff5c8aa174fe4eccde9188
Ankon 190 <ankon190@gmail.com>
2026-08-07 10:05:42 +0600

    feat: add global error handling middleware

--- erp-server/src/shared/middleware/error.middleware.js
+++ erp-server/src/shared/middleware/error.middleware.js
@@ -1,6 +1,5 @@
 import ApiError from "../utils/ApiError.js";
 
-
 const errorHandler = (err, req, res, next) => {
   let error = err;
 
--- erp-server/src/shared/utils/asyncHandler.js
+++ erp-server/src/shared/utils/asyncHandler.js
@@ -1,6 +1,6 @@
-const asyncHandler = (handler) => {
+const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
-    Promise.resolve(handler(req, res, next)).catch(next);
+    Promise.resolve(requestHandler(req, res, next)).catch(next);
   };
 };
 
